c9e5b371a2db95ef0be537c49664e225c81565cf
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-03-27 16:17:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,25 +13,56 @@
 #include "push_swap.h"
 #include <stdio.h>
 
+void	sa(Stack stack_a);
+void	sort_numbers(Stack stack_a, Stack stack_b);
+
 int main(int argc, char *argv[])
 {
-	int i;
+    Stack   *stack_a;
+    Stack   *stack_b;
+    Stack   *temp;
+    int		i;
 
 	i = 1;
-	if (argc == 1)
-	{
+	if (argc < 2)
 		return (1);
-	}
-	while (argv[i])
+	temp = (Stack *)malloc(sizeof(Stack));
+	stack_a = (Stack *)malloc(sizeof(Stack));
+	stack_b = (Stack *)malloc(sizeof(Stack));
+	while (argc > i) // mientras que el numero de argumentos - 1 sea mayor que 0, guardaré los argumentos en el stack temp
 	{
-		ft_atoi(argv[i]);
-		printf("%s\n", argv[i]);
+		temp->value = ft_atoi(argv[i]);
+		temp->next = stack_a;
+		stack_a = temp;
 		i++;
 	}
-
-	int c[STACK_LENGTH]; // initialize the stack
-	Stack * s = stack(STACK_LENGTH); //not sure what s is
+	sort_numbers(stack_a, stack_b);
 	return (0);
 }
+
+void	sort_numbers(Stack stack_a, Stack stack_b)
+{
+	Stack	*temp;
+
+	while (stack_a->value != NULL)
+    {
+        while (stack_a->value > stack_a->next)
+        {
+			sa(stack_a);
+        }
+    }
+}
+
+void	sa(Stack stack_a)
+{
+	Stack *temp;
+
+	temp = (Stack *)malloc(sizeof(Stack));
+	temp = stack_a;
+	stack_a->value = temp->next;
+	stack_a->next = temp->value;
+	free(temp);
+}
+
 // to define a stack https://www.digitalocean.com/community/tutorials/stack-in-c
 // another option to define a stack https://dev.to/josethz00/data-structures-in-c-stack-55c7#:~:text=Stack%20implementation%20(struct),as%20the%20primitive%20type%20char%20.
