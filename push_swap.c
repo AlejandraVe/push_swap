4765143560f988f093c00bc08f8a714faf3aac9e
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-03-28 16:38:52 +0100

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -21,32 +21,61 @@ int main(int argc, char *argv[])
 	bool	syntax; // bool can be either true or false
     int		i;
 	int		j;
-	int		n;
 
 	i = 1;
 	j = 0;
-	n = 1;
-	if (argc < 2 || !argv[1][0])
-		return (1);
+	syntax = true;
 	temp = (Stack *)malloc(sizeof(Stack));
 	stack_a = (Stack *)malloc(sizeof(Stack));
 //	stack_b = (Stack *)malloc(sizeof(Stack));
-	if (argc == 2) // this means the given numbers are a string, so we need to split it
+	if (argc < 2 || !argv[1][0])
+		return (1);
+	else if (argc == 2) // this means the given numbers are a string, so we need to split it
 	{
+		if (argv[1][j] == '"')
+			j++;
+		while (argv[1][j])
+		{
+			syntax = check_char(argv[1][j]);
+			if (syntax == false)
+			{
+				printf("Error\n");
+				return (1);
+				break;
+			};
+			j++;
+		}
 		char	**string; // doble puntero porque es un array de strings
 		string = ft_split(argv[1], ' ');
-		while (string[j])
+		j = 0;
+		while (string[j] && syntax == true)
 		{
 			argv[j + 1] = string[j]; // I store every string of array "string" in argv[j + 1], so that it's in position 1
 			j++;
-			//printf("%s\n", argv[j + 1]);
 		}
+			//printf("%s\n", argv[j + 1]);
 	}
-	while (argv[n])
+	else if (argc > 2)
 	{
-		syntax = check_syntax(argv[n]);
-		if (syntax == false)
-			break;
+		int n = 1;
+		int counter = 0;
+
+		while (argv[n][counter])
+		{
+			while (argv[n][counter])
+			{
+				syntax = check_char(argv[n][counter]);
+				printf("%d\n", syntax);
+				if (syntax == false)
+				{
+					printf("Error\n");
+					return (1);
+					break;
+				}
+				counter++;
+			}
+			n++;
+		}
 	}
 	if (syntax == true)
 	{
@@ -55,50 +84,46 @@ int main(int argc, char *argv[])
 			temp->value = ft_atoi(argv[i]);
 			temp->next = stack_a;
 			stack_a = temp;
-			printf("%d\n", stack_a->value);
+	//		printf("%d\n", stack_a->value);
 			i++;
 		}
-	}
-	else
-	{
-		printf("Error\n");
 	}
 //	sort_numbers(stack_a, stack_b);
 	free (temp);
 	return (0);
 }
 
-bool    check_syntax(char *argv)
+bool	check_char(char c)
+{
+	if (ft_isdigit(c) || c == ' ' || c == '-' || c == '+')
+		return (1);
+	else
+		return (0);
+}
+
+/*bool    check_syntax(char *argv)
 {
 	int		i;
 //	int		j;
 	bool	ret;
 
-	ret = true;
-	i = 0; // because i want to check the info from the argument value 1st position
+	ret = false;
+	i = 0;
 //	j = 0;
 	while (argv[i])
 	{
-		if ((ft_isdigit(argv[i]) || (argv[i][j] != ' ' || argv[i][j] != '-' || argv[i][j] != '\t'
-			|| argv[i][j] != '\n' || argv[i][j] != '\r' || argv[i][j] != '\f' || argv[i][j] != '\v')))
+		if ((ft_isdigit(argv[i]) || (argv[i] == ' ' || argv[i] == '-' || argv[i] == '\t'
+			|| argv[i] == '\n' || argv[i] == '\r' || argv[i] == '\f' || argv[i] == '\v')))
 		{
 			ret = true;
 		}
-		else
-		{
-			ret = false;
-			break;
-		}
-	i++;
+		i++;
 	}
 	return (ret);
 }
 
-/*void	sort_numbers(Stack stack_a, Stack stack_b)
+void	sort_numbers(Stack stack_a, Stack stack_b)
 {
 }*/
 
 
-
-// to define a stack https://www.digitalocean.com/community/tutorials/stack-in-c
-// another option to define a stack https://dev.to/josethz00/data-structures-in-c-stack-55c7#:~:text=Stack%20implementation%20(struct),as%20the%20primitive%20type%20char%20.
