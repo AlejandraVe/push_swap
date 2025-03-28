123d2bd5f7069a0402e5a6eaf002fefdbed3f10a
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-03-28 15:31:32 +0100

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -15,20 +15,90 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+    Stack   *stack_a;
+ //   Stack   *stack_b;
+    Stack   *temp;
+	bool	syntax; // bool can be either true or false
+    int		i;
+	int		j;
+	int		n;
 
 	i = 1;
-	if (argc == 1)
-	{
+	j = 0;
+	n = 1;
+	if (argc < 2 || !argv[1][0])
 		return (1);
-	}
-	while (argv[i])
+	temp = (Stack *)malloc(sizeof(Stack));
+	stack_a = (Stack *)malloc(sizeof(Stack));
+//	stack_b = (Stack *)malloc(sizeof(Stack));
+	if (argc == 2) // this means the given numbers are a string, so we need to split it
 	{
-		ft_atoi(argv[i]);
-		printf("%s\n", argv[i]);
-		i++;
+		char	**string; // doble puntero porque es un array de strings
+		string = ft_split(argv[1], ' ');
+		while (string[j])
+		{
+			argv[j + 1] = string[j]; // I store every string of array "string" in argv[j + 1], so that it's in position 1
+			j++;
+			//printf("%s\n", argv[j + 1]);
+		}
 	}
+	while (argv[n])
+	{
+		syntax = check_syntax(argv[n]);
+		if (syntax == false)
+			break;
+	}
+	if (syntax == true)
+	{
+		while (j + 1 > i || argc > i) // mientras que el numero de argumentos - 1 sea mayor que 0, guardaré los argumentos en el stack temp
+		{
+			temp->value = ft_atoi(argv[i]);
+			temp->next = stack_a;
+			stack_a = temp;
+			printf("%d\n", stack_a->value);
+			i++;
+		}
+	}
+	else
+	{
+		printf("Error\n");
+	}
+//	sort_numbers(stack_a, stack_b);
+	free (temp);
 	return (0);
 }
+
+bool    check_syntax(char *argv)
+{
+	int		i;
+//	int		j;
+	bool	ret;
+
+	ret = true;
+	i = 0; // because i want to check the info from the argument value 1st position
+//	j = 0;
+	while (argv[i])
+	{
+		if ((ft_isdigit(argv[i]) || (argv[i][j] != ' ' || argv[i][j] != '-' || argv[i][j] != '\t'
+			|| argv[i][j] != '\n' || argv[i][j] != '\r' || argv[i][j] != '\f' || argv[i][j] != '\v')))
+		{
+			ret = true;
+		}
+		else
+		{
+			ret = false;
+			break;
+		}
+	i++;
+	}
+	return (ret);
+}
+
+/*void	sort_numbers(Stack stack_a, Stack stack_b)
+{
+}*/
+
+
+
 // to define a stack https://www.digitalocean.com/community/tutorials/stack-in-c
 // another option to define a stack https://dev.to/josethz00/data-structures-in-c-stack-55c7#:~:text=Stack%20implementation%20(struct),as%20the%20primitive%20type%20char%20.
