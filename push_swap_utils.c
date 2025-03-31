7357206450e344af856106bd9c3b5867738c9f77
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-03-31 13:36:55 +0200

    Update push_swap_utils.c

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -168,3 +168,27 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ret[i] = '\0';
 	return (ret);
 }
+
+int	count_string(char **s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = '\0';
+		i++;
+	}
+}
