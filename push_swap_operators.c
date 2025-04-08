63791edef40b9648efc84872600b77a696a5c005
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-08 12:58:37 +0200

    Update push_swap_operators.c

--- push_swap_operators.c
+++ push_swap_operators.c
@@ -13,9 +13,62 @@ static void	swap(Stack **top) // swaps the position of the top and second node
 	(*top)->prev = NULL; // this is to complete the swap
 }
 
-void swap_a(Stack **stack_a, bool print) // print the instruction
+static void rotate(Stack **stack)
+{
+	Stack	*last_node;
+
+	last_node = find_last(*stack);
+	last_node->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
+}
+
+static void	reverse_rotate(Stack **stack)
+{
+	Stack	*last;
+
+	last = find_last(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
+}
+
+void	sa(Stack **stack_a, bool print) // print the instruction
 {
 	swap(stack_a);
 	if (print)
 		write(1, "sa\n", 3);
+}
+
+void	ra(Stack **stack_a, bool print)
+{
+	rotate(stack_a);
+	if (print)
+		write(1, "ra\n", 3);
+}
+
+void	rb(Stack **stack_b, bool print)
+{
+	rotate(stack_b);
+	if (print)
+		write(1, "rb\n", 3);
+}
+
+void	rr(Stack **stack_a, Stack **stack_b, bool print)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	if (print)
+		write(1, "rr\n", 3);
+}
+
+void	rra(Stack **stack_a, bool print)
+{
+	reverse_rotate(stack_a);
+	if (print)
+		write(1, "rra\n", 4);
 }
