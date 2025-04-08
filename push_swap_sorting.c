7a0d81521a3c0f9c5bde5c93ff9b491ea1efa132
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-08 12:57:59 +0200

    Update push_swap_sorting.c

--- push_swap_sorting.c
+++ push_swap_sorting.c
@@ -1,56 +1,36 @@
 #include "push_swap.h"
 #include <stdio.h>
 
-static void    append_node(Stack **stack, int n)
+void    sort_three_nodes(Stack **stack_a)
 {
-    Stack   *node; // store a pointer to the new node to be created with the value n
-    Stack   *last_node; // to store a pointer to the current last node of the stack
+    Stack   *max_node;
 
-    if (!stack)
-        return ;
-    node = malloc(sizeof(Stack));
-    if (!node)
-        return ;
-    node->next = NULL; // set the next pointer to NULL as it'll be the last node of the lost
-    node->value = n; // set the value to the number the function receives
-    node->cheapest = 0; // initialise the cheapest to 0
-    if (!(*stack)) // check if the stack is empty or currently pointing to NULL; this would indicate a 1st node needs to be found
-    {
-        *stack = node; //if it's empty, we set the tack to point to the node, making it the new head of the stack
-        node->prev = NULL; //set the head node's previous pointer to NULL as it's the first node
-    }
-    else
-    {
-        last_node = find_last(*stack);
-        last_node->next = node;
-        node->prev = last_node;
-    }
-    printf("%d\n", node->value);
+    max_node = find_max(*stack_a);
+    if (max_node == *stack_a)
+        ra(stack_a, true);
+    else if (max_node == (*stack_a)->next)
+        rra(stack_a, true);
+    if ((*stack_a)->value > (*stack_a)->next->value)
+        sa(stack_a, true);
 }
 
-void    initialize_stack_a(Stack **stack_a, char *string[])
+Stack    *find_max(Stack *stack)
 {
-    //	int number_of_strings;
-	int n;
-    int i;
-    //	number_of_strings = count_string(string) - 1;
-    i = 0;
-    while (string[i])
-    {
-        n = ft_atoi(string[i]);
-        append_node(stack_a, n);
-        i++;
-    }
-    free(string);
-}
+    Stack   *max_node;
+    int     max_value;
 
-Stack	*find_last(Stack *stack) //Define a function that returns the pointer to the last node
-{
-	if (!stack)
-		return (NULL);
-	while (stack->next) //Loop until the end of the stack is reached
-		stack = stack->next;
-	return (stack);
+    max_value = 0;
+    while (stack)
+    {
+        if (stack->value > max_value)
+        {
+            max_value = stack->value;
+            max_node = stack;
+        }
+        stack = stack->next;
+    }
+    printf("The max value is %d\n", max_value);
+    return (max_node);
 }
 
 bool    check_sorted(Stack *stack)
@@ -69,11 +49,14 @@ bool    check_sorted(Stack *stack)
     }
 }
 
-void	sort_numbers(Stack *stack_a)
+void	sort_numbers(Stack **stack_a, Stack **stack_b)
 {
     Stack   *last_node = NULL;
 
+    if (!stack_a || !stack_b)
+        return ;
     last_node = (Stack *)malloc(sizeof(Stack));
-    last_node = find_last(stack_a);
+    last_node = find_last(*stack_a);
     printf("%d\n", last_node->value);
 }
+
