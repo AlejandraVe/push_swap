b480ae7f2037c248656d988fd59d084a4b158c52
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-14 18:25:51 +0200

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -56,6 +56,8 @@ Stack				*find_max(Stack *stack);
 Stack				*find_min(Stack *stack);
 void				sort_three_nodes(Stack **stack_a);
 void    			check_index(Stack *stack);
+void				check_cost(Stack *stack_a, Stack *stack_b);
+void    			set_cheapest(Stack *stack);
 void				rr(Stack **stack_a, Stack **stack_b, bool print);
 void				rb(Stack **stack_b, bool print);
 void				ra(Stack **stack_a, bool print);
