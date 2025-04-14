704841596e17e7bfe72daee28b340a88594d6539
Alejandra Vera Vela <alvera-v@c3r6s2.42malaga.com>
2025-04-14 12:44:27 +0200

    added check_target_nodes function

--- push_swap.h
+++ push_swap.h
@@ -6,61 +6,65 @@
 /*   By: alvera-v <alvera-v@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:05:05 by alvera-v          #+#    #+#             */
-/*   Updated: 2025/04/14 11:29:50 by alvera-v         ###   ########.fr       */
+/*   Updated: 2025/04/14 12:14:37 by alvera-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <limits.h> // Includes int-max and int-min
 # include <stdbool.h>
+# include <stddef.h> // Includes typedef
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h> // Includes int-max and int-min
-# include <stddef.h> // Includes typedef
 
-typedef struct stack {
-	int             value;
-    int             index;
-    int             push_cost;
-    bool            above_median;
-    bool            cheapest;
-    struct stack    *next; // el último del stack, su next debe apuntar a nulo o void.
-    struct stack    *prev; // tengo que comprobar p¡con prev, que el prev del valor al que estoy apuntando sea NULL
-    struct stack    *target_node;
-} Stack;
-int     main(int argc, char *argv[]);
-int     ft_atoi(const char *nptr);
-int     ft_isdigit(int c);
-char	**ft_split(char const *s, char c);
-size_t	ft_strlen(char const *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-bool	check_char(char c);
-int     count_string(char **s);
-bool    check_duplicates(char **s);
-char	**many_strings(char *argv[]);
-char	**handle_errors(int argc, char *argv[]);
-//char	*ft_strtrim(char const *s1, char const *set);
-//void	*ft_calloc(size_t nmeb, size_t size);
-//char	*ft_strdup(const char *s);
-//void	ft_bzero(void *s, size_t n);
-//size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-void	sort_numbers(Stack **stack_a, Stack **stack_b);
-Stack	*find_last(Stack *stack);
-void    initialize_stack_a(Stack **stack_a, char *string[]);
-bool    check_sorted(Stack *stack);
-int     stack_len(Stack *stack);
-Stack   *find_max(Stack *stack);
-void    sort_three_nodes(Stack **stack_a);
-void	rr(Stack **stack_a, Stack **stack_b, bool print);
-void	rb(Stack **stack_b, bool print);
-void	ra(Stack **stack_a, bool print);
-void	rra(Stack **stack_a, bool print);
-void	rrb(Stack **stack_b, bool print);
-void	rrr(Stack **stack_a, Stack **stack_b, bool print);
-void    sa(Stack **stack_a, bool print);
-void	sb(Stack **stack_b, bool print);
-void	ss(Stack **stack_a, Stack **stack_b, bool print);
-void	pa(Stack **stack_a, Stack **stack_b, bool print);
-void	pb(Stack **stack_b, Stack **stack_a, bool print);
+typedef struct stack
+{
+	int				value;
+	int				index;
+	int				push_cost;
+	bool			above_median;
+	bool			cheapest;
+	struct stack *next; // el último del stack,
+		//su next debe apuntar a nulo o void.
+	struct stack *prev; // tengo que comprobar p¡con prev,
+		//que el prev del valor al que estoy apuntando sea NULL
+	struct stack	*target_node;
+}					Stack;
+int					main(int argc, char *argv[]);
+int					ft_atoi(const char *nptr);
+int					ft_isdigit(int c);
+char				**ft_split(char const *s, char c);
+size_t				ft_strlen(char const *s);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+bool				check_char(char c);
+int					count_string(char **s);
+bool				check_duplicates(char **s);
+char				**many_strings(char *argv[]);
+char				**handle_errors(int argc, char *argv[]);
+// char	*ft_strtrim(char const *s1, char const *set);
+// void	*ft_calloc(size_t nmeb, size_t size);
+// char	*ft_strdup(const char *s);
+// void	ft_bzero(void *s, size_t n);
+// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void				sort_numbers(Stack **stack_a, Stack **stack_b);
+Stack				*find_last(Stack *stack);
+void				initialize_stack_a(Stack **stack_a, char *string[]);
+bool				check_sorted(Stack *stack);
+int					stack_len(Stack *stack);
+Stack				*find_max(Stack *stack);
+Stack				*find_min(Stack *stack);
+void				sort_three_nodes(Stack **stack_a);
+void				rr(Stack **stack_a, Stack **stack_b, bool print);
+void				rb(Stack **stack_b, bool print);
+void				ra(Stack **stack_a, bool print);
+void				rra(Stack **stack_a, bool print);
+void				rrb(Stack **stack_b, bool print);
+void				rrr(Stack **stack_a, Stack **stack_b, bool print);
+void				sa(Stack **stack_a, bool print);
+void				sb(Stack **stack_b, bool print);
+void				ss(Stack **stack_a, Stack **stack_b, bool print);
+void				pa(Stack **stack_a, Stack **stack_b, bool print);
+void				pb(Stack **stack_b, Stack **stack_a, bool print);
 
 #endif
--- push_swap_sorting.c
+++ push_swap_sorting.c
@@ -33,6 +33,26 @@ Stack	*find_max(Stack *stack)
 	return (max_node);
 }
 
+Stack	*find_min(Stack *stack)
+{
+	Stack	*min_node;
+	int		min_value;
+
+	min_node = (find_max(stack));
+	min_value = min_node->value;
+	while (stack)
+	{
+		if (stack->value < min_value)
+		{
+			min_value = stack->value;
+			min_node = stack;
+		}
+		stack = stack->next;
+	}
+	printf("The min value is %d\n", min_value);
+	return (min_node);
+}
+
 bool	check_sorted(Stack *stack)
 {
 	if (!stack)
@@ -49,6 +69,41 @@ bool	check_sorted(Stack *stack)
 	}
 }
 
+static void	check_target_node(Stack *stack_a, Stack *stack_b)
+{
+	Stack	*min_node;
+	Stack	*max_node;
+	Stack	*target;
+	int		target_node_int;
+	int		min_value;
+
+	min_node = find_min(stack_b);
+	max_node = find_max(stack_b);
+	target_node_int = min_node->value;
+	while (stack_a)
+	{
+		while (stack_b)
+		{
+			if (stack_a->value > stack_b->value
+				&& stack_a->value >= target_node_int)
+			{
+				target = stack_b;
+				target_node_int = stack_b->value;
+			}
+			else
+			{
+				target = find_max(stack_b);
+				target_node_int = max_node->value;
+			}
+			stack_b = stack_b->next;
+		}
+        stack_a->target_node = target;
+		stack_a = stack_a->next;
+	}
+	min_value = min_node->value;
+	printf("Min value after checking target nodes: %d\n", min_value);
+}
+
 void	sort_numbers(Stack **stack_a, Stack **stack_b)
 {
 	Stack	*top_a;
@@ -63,6 +118,11 @@ void	sort_numbers(Stack **stack_a, Stack **stack_b)
 			pa(stack_a, stack_b, true);
 			pa(stack_a, stack_b, true);
 		}
+		check_target_node(*stack_a, *stack_b);
+		top_a = *stack_a;
+		top_b = *stack_b;
+		printf("Top of stack_a: %d and top of stack_b: %d\n", top_a->value,
+			top_b->value);
 	}
 	if (stack_len(*stack_a) == 3)
 	{
