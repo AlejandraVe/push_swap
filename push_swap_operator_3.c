a96174571337efe95e4035d750d1ccd06b1e64f6
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-17 16:21:35 +0200

    Update push_swap_operator_3.c

--- push_swap_operator_3.c
+++ push_swap_operator_3.c
@@ -1,71 +1,68 @@
 #include "push_swap.h"
+#include <stdio.h>
 
-static void	check_b_position(Stack *a, Stack *b, Stack *cheapest)
+static Stack	*check_b_position(Stack *a, Stack *b, Stack *cheapest) // I also have to check if the cheapest is in the second position; if so, i just need to swap
 {
-	if (cheapest->target_node->above_median)
+	Stack *top_b;
+
+	while (b != cheapest->target_node || a != cheapest)
 	{
-		while (b != cheapest->target_node || a != cheapest)
+		if (b != cheapest->target_node && a == cheapest)
+        {
+			if (b->next == cheapest->target_node)
+                sb(&b, true);
+            else
+                rb(&b, true);
+        }
+		if (b == cheapest->target_node && a != cheapest)
 		{
-			if (b != cheapest->target_node && a == cheapest)
-				rb(&b, true);
-			else if (b == cheapest->target_node && a != cheapest)
+			if (a->next == cheapest)
+				sa(&a, true);
+			else
 				ra(&a, true);
 		}
-	}
-	else if (!cheapest->target_node->above_median)
-	{
-		while (b != cheapest->target_node || a != cheapest)
-		{
-			if (b != cheapest->target_node && a == cheapest)
-				rrb(&b, true);
-			else if (b == cheapest->target_node && a != cheapest)
-				rra(&a, true);
-		}
-	}
-	check_index(a);
-	check_index(b);
+    }
+	top_b = b;
+	printf("in function check_b_position, top of stack_b: %d\n", top_b->value);
+	return (top_b);
 }
 
-/*static void    check_b_position_reverse(Stack *a, Stack *b, Stack *cheapest)
+static Stack	*check_rev_b_position(Stack *a, Stack *b, Stack *cheapest)
 {
+    Stack *top_b;
+
 	while (b != cheapest->target_node || a != cheapest)
 	{
 		if (b != cheapest->target_node && a == cheapest)
 			rrb(&b, true);
 		else if (b == cheapest->target_node && a != cheapest)
 			rra(&a, true);
-	}
-	check_index(a);
-	check_index(b);
-}*/
-void	check_rotation_both(Stack *a, Stack *b, Stack *cheapest)
-{
-	while (b != cheapest->target_node && a != cheapest)
-		rr(&a, &b, true);
-	/*while (1)
-	{
-		if (b == cheapest->target_node && a != cheapest)
-			ra(&a, true);
-		else if (b != cheapest->target_node && a == cheapest)
-			rb(&b, true);
-		else if (b == cheapest->target_node && a == cheapest)
-			break ;
-	}*/
-	check_b_position(a, b, cheapest);
+    }
+	top_b = b;
+	printf("in function check_rev_b_position, top of stack_b: %d\n", top_b->value);
+	return (top_b);
 }
 
-void	check_rev_rotation_both(Stack *a, Stack *b, Stack *cheapest)
+Stack	*check_rotation_both(Stack *a, Stack *b, Stack *cheapest) // target node and cheapet are above median
 {
+	Stack *top_b;
+
+	while (b != cheapest->target_node && a != cheapest)
+	{
+		if (a->next == cheapest && b->next == cheapest->target_node)
+			ss(&a, &b, true);
+		else
+			rr(&a, &b, true);
+	}
+	top_b = check_b_position(a, b, cheapest);
+	return (top_b);
+}
+
+Stack	*check_rev_rotation_both(Stack *a, Stack *b, Stack *cheapest) // target node and cheapest are below median
+{
+	Stack *top_b;
 	while (b != cheapest->target_node && a != cheapest)
 		rrr(&a, &b, true);
-	/* while (1)
-		{
-			if (b == cheapest->target_node && a != cheapest)
-				rra(&a, true);
-			else if (b != cheapest->target_node && a == cheapest)
-				rrb(&b, true);
-			else if (b == cheapest->target_node && a == cheapest)
-				break ;
-		}*/
-	check_b_position(a, b, cheapest);
+	top_b = check_rev_b_position(a, b, cheapest);
+	return (top_b);
 }
