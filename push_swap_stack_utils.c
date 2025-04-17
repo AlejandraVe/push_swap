84927257759b122c167caefe0484745641470113
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-17 16:24:45 +0200

    Update push_swap_stack_utils.c

--- push_swap_stack_utils.c
+++ push_swap_stack_utils.c
@@ -1,4 +1,5 @@
 #include "push_swap.h"
+#include <stdio.h>
 
 int	stack_len(Stack *stack)
 {
@@ -28,5 +29,34 @@ void	check_index(Stack *stack)
 			stack->above_median = false;
 		stack = stack->next;
 		i++;
+	}
+}
+
+void	check_target_node_a(Stack *stack_a, Stack *stack_b)
+{
+	Stack	*top_b;
+	Stack	*target;
+	long	target_node_int;
+
+	while (stack_a)
+	{
+		target_node_int = LONG_MIN;
+		top_b = stack_b;
+		while (top_b)
+		{
+			if (top_b->value < stack_a->value && top_b->value > target_node_int)
+			{
+				target_node_int = top_b->value;
+				target = top_b;
+			}
+			top_b = top_b->next;
+		}
+		if (target_node_int == LONG_MIN)
+			stack_a->target_node = find_max(stack_b);
+		else
+			stack_a->target_node = target;
+		printf("target node of top of a %d is: %d\n", stack_a->value,
+			stack_a->target_node->value);
+		stack_a = stack_a->next;
 	}
 }
