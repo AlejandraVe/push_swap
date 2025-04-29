12a1929f24a4a7b1e6f472c6e61cf114110c1a2b
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-29 18:31:00 +0200

    works

--- push_swap_operators_4.c
+++ push_swap_operators_4.c
@@ -1,11 +1,9 @@
 #include "push_swap.h"
-#include <stdio.h>
 
 void	check_rotation_a_above(Stack **a, Stack **b, Stack *cheapest)
 {
-    Stack *top_b;
-	Stack *top_a;
-
+	if (!a)
+		return ;
 	while (*b != cheapest->target_node || *a != cheapest)
 	{
 		if (*b != cheapest->target_node && *a != cheapest)
@@ -17,21 +15,15 @@ void	check_rotation_a_above(Stack **a, Stack **b, Stack *cheapest)
 			rrb(b, true);
 		if (*b == cheapest->target_node && *a != cheapest)
 		{
-			top_a = *a;
-			if (top_a->next == cheapest)
-				sa(a, true);
-			else
-				ra(a, true);
+			ra(a, true);
 		}
     }
-	top_b = *b;
-	printf("in function check_rotation_a_above, top of stack_b: %d\nand the next of stack_b is %d\n", top_b->value, top_b->next->value);
 }
 
 void	check_rotation_b_above(Stack **a, Stack **b, Stack *cheapest)
 {
-    Stack *top_b;
-
+	if (!a)
+		return ;
 	while (*b != cheapest->target_node || *a != cheapest)
 	{
 		if (*b != cheapest->target_node && *a != cheapest)
@@ -43,13 +35,7 @@ void	check_rotation_b_above(Stack **a, Stack **b, Stack *cheapest)
 			rra(a, true);
 		if (*a == cheapest && *b != cheapest->target_node)
 		{
-			top_b = *b;
-			if (top_b->next == cheapest->target_node)
-				sb(b, true);
-			else
-				rb(b, true);
+			rb(b, true);
 		}
     }
-	top_b = *b;
-	printf("in function check_rotation_b_above, top of stack_b: %d\nand the next of stack_b is %d\n", top_b->value, top_b->next->value);
 }
