cfa587274bae7a3b68605cfd2b451c6b711d58ce
AlejandraVe <152411557+AlejandraVe@users.noreply.github.com>
2025-04-29 18:31:19 +0200

    works

--- push_swap_sorting.c
+++ push_swap_sorting.c
@@ -5,6 +5,8 @@ void	sort_three_nodes(Stack **stack_a)
 {
 	Stack	*max_node;
 
+	if (!stack_a)
+		return ;
 	max_node = find_max(*stack_a);
 	if (max_node == *stack_a)
 		ra(stack_a, true);
@@ -17,9 +19,11 @@ void	sort_three_nodes(Stack **stack_a)
 Stack	*find_max(Stack *stack)
 {
 	Stack	*max_node;
-	int		max_value;
+	long	max_value;
 
-	max_value = 0;
+	if (!stack)
+		return (NULL);
+	max_value = INT_MIN;
 	while (stack)
 	{
 		if (stack->value > max_value)
@@ -37,6 +41,8 @@ Stack	*find_min(Stack *stack)
 	Stack	*min_node;
 	int		min_value;
 
+	if (!stack)
+		return (NULL);
 	min_node = (find_max(stack));
 	min_value = min_node->value;
 	while (stack)
@@ -70,9 +76,9 @@ bool	check_sorted(Stack *stack)
 static void	move_a_to_b(Stack **stack_a, Stack **stack_b)
 {
 	Stack	*cheapest;
-	Stack	*top_a;
-	Stack	*top_b;
 
+	if (!stack_a)
+		return ;
 	cheapest = store_cheapest(*stack_a);
 	if (cheapest->above_median && cheapest->target_node->above_median)
 		check_rotation_both(stack_a, stack_b, cheapest);
@@ -82,10 +88,6 @@ static void	move_a_to_b(Stack **stack_a, Stack **stack_b)
         check_rotation_a_above(stack_a, stack_b, cheapest);
 	else if (!(cheapest->above_median) && cheapest->target_node->above_median)
 		check_rotation_b_above(stack_a, stack_b, cheapest);
-    top_a = *stack_a;
-    top_b = *stack_b;
-	printf("Top of stack_a: %d and top of stack_b: %d\n", top_a->value,
-		top_b->value);
 }
 
 static void	move_b_to_a(Stack **a, Stack **b)
@@ -93,8 +95,6 @@ static void	move_b_to_a(Stack **a, Stack **b)
 	Stack	*cheapest;
 
 	cheapest = store_cheapest(*b);
-	printf("Cheapest from b has been stored\n");
-	printf("Above median: %d %d\n", cheapest->above_median, cheapest->target_node->above_median);
 	if (cheapest->above_median && cheapest->target_node->above_median)
 		b_to_a_both_above(a, b, cheapest);
 	else if (!cheapest->above_median && !cheapest->target_node->above_median)
@@ -110,8 +110,9 @@ static void	move_b_to_a(Stack **a, Stack **b)
 void	sort_numbers(Stack **stack_a, Stack **stack_b)
 {
 	Stack	*top_a;
-	Stack	*top_b;
 
+	if (!stack_a)
+		return ;
 	if (stack_len(*stack_a) == 3)
 	{
 		sort_three_nodes(stack_a);
@@ -130,48 +131,37 @@ void	sort_numbers(Stack **stack_a, Stack **stack_b)
 			break;
 		check_properties(*stack_a, *stack_b);
 		move_a_to_b(stack_a, stack_b);
-		top_a = *stack_a;
-		top_b = *stack_b;
-		printf("After pushing a to b: Top of stack_a: %d and next is: %d\n And top of stack_b: %d and next is %d\n", top_a->value, top_a->next->value,
-			top_b->value, top_b->next->value);
 	}
 	sort_three_nodes(stack_a);
 	while (stack_len(*stack_b) > 0)
 	{
 		begin_sort_b(*stack_a, *stack_b);
 		move_b_to_a(stack_a, stack_b);
-		printf("stack_len of b: %d\nstack_len of a: %d\n", stack_len(*stack_b), stack_len(*stack_a));
 		check_index(*stack_a);
 		if (!(*stack_b))
 			break;
 		else
 			check_index(*stack_b);
 	}
-	top_a = *stack_a;
-	for (int i = 0; i < stack_len(*stack_a); i++)
-	{
-		printf("stack_a posi %d: %d\n", i, top_a->value);
-		top_a = top_a->next;
-	}
+	check_index(*stack_a);
 	min_at_top(stack_a);
 	top_a = *stack_a;
 	for (int i = 0; i < stack_len(*stack_a); i++)
 	{
-		printf("stack_a posi %d: %d\n", i, top_a->value);
+		printf("End: stack a posci %d: %d\n", i, top_a->value);
 		top_a = top_a->next;
 	}
 }
 
 void	min_at_top(Stack **stack_a)
 {
-	Stack	*min;
-
-	min = find_min(*stack_a);
-	while (min != *stack_a)
+	if (!stack_a)
+		return ;
+	while (find_min(*stack_a)->value != (*stack_a)->value)
 	{
-		if (!min->above_median)
-			rra(stack_a, true);
-		else
+		if (find_min(*stack_a)->above_median)
 			ra(stack_a, true);
+		else
+			rra(stack_a, true);
 	}
 }
